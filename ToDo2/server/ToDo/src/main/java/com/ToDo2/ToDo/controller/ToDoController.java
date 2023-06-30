d34dbd41a5683a1013ad3e677e895e72a1029027
Sahil Gedam <gedamsahil1997@gmail.com>
2023-06-30 12:08:21 +0530

    Mockito Test cases added

--- ToDo2/server/ToDo/src/main/java/com/ToDo2/ToDo/controller/ToDoController.java
+++ ToDo2/server/ToDo/src/main/java/com/ToDo2/ToDo/controller/ToDoController.java
@@ -6,23 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ToDo2.ToDo.entities.Tasks;
 import com.ToDo2.ToDo.services.ToDoServices;
 
-@RestController
+@RestController 
 public class ToDoController {
 	@Autowired
 	private ToDoServices toDoServices;
 
 // fetch tasks
-	@RequestMapping(value = "/tasks")
+	@GetMapping(value = "/tasks")
 	public List<Tasks> getAllTasks() {
 		return toDoServices.getAllTasks();
 	}
@@ -47,8 +47,8 @@ public class ToDoController {
 
 // update status as complete
 	@PutMapping(value = "/complete/{id}")
-	public ResponseEntity<Object> updateStatus(@PathVariable long id, @RequestBody Tasks task) {
-		toDoServices.updateStatus(id, task);
+	public ResponseEntity<Object> updateStatus(@PathVariable long id) {
+		toDoServices.updateStatus(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 
 	}
--- ToDo2/server/ToDo/src/main/java/com/ToDo2/ToDo/services/ToDoServices.java
+++ ToDo2/server/ToDo/src/main/java/com/ToDo2/ToDo/services/ToDoServices.java
@@ -2,14 +2,11 @@ package com.ToDo2.ToDo.services;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,7 +32,7 @@ public class ToDoServices {
 				tasksList.get(i).setStatus(false);
 			}
 
-			// logic to calculate remaining days and send them in date column
+			// logic to calculate remaining days and send them in time column
 			try {
 				Date dateCurrentInFormat = new SimpleDateFormat("yyyy-MM-dd").parse(dateCurrent);
 
@@ -56,34 +53,43 @@ public class ToDoServices {
 		return tasksList;
 	}
 
-	public ResponseEntity<Object> saveTasks(@RequestBody Tasks task) {
+	public Tasks saveTasks(@RequestBody Tasks newTaskBody) {
 
-		tasksRepo.save(task);
+		tasksRepo.save(newTaskBody);
 
-		return new ResponseEntity<>(HttpStatus.OK);
+		return newTaskBody;
+
 	}
 
-	public ResponseEntity<Object> updateTasks(@PathVariable long id, @RequestBody Tasks task) {
+	public Tasks updateTasks(@PathVariable long id, @RequestBody Tasks newTaskBody) {
 
-		Tasks updatedTask = tasksRepo.findById(id).get();
-		updatedTask.setTask(task.getTask());
-		tasksRepo.save(updatedTask);
-		return new ResponseEntity<>(HttpStatus.OK);
+		Tasks taskToUpdate = tasksRepo.findById(id).get();
+
+		taskToUpdate.setTask(newTaskBody.getTask());
+
+		tasksRepo.save(taskToUpdate);
+
+		return taskToUpdate;
+
 	}
 
-	public ResponseEntity<Object> updateStatus(@PathVariable long id, @RequestBody Tasks task) {
+	public Tasks updateStatus(@PathVariable long id) {
 
-		Tasks updatedTask = tasksRepo.findById(id).get();
+		Tasks taskToUpdate = tasksRepo.findById(id).get();
 
-		updatedTask.setStatus(false);
+		taskToUpdate.setStatus(false);
 
-		tasksRepo.save(updatedTask);
-		return new ResponseEntity<>(HttpStatus.OK);
+		tasksRepo.save(taskToUpdate);
+
+		return taskToUpdate;
+
 	}
 
-	public ResponseEntity<Object> deleteTask(@PathVariable long id) {
-		Tasks deleteTask = tasksRepo.findById(id).get();
-		tasksRepo.delete(deleteTask);
-		return new ResponseEntity<>(HttpStatus.OK);
+	public void deleteTask(@PathVariable long id) {
+
+		Tasks taskToDelete = tasksRepo.findById(id).get();
+
+		tasksRepo.delete(taskToDelete);
+
 	}
 }
