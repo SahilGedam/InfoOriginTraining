6fc91a42a2c23f3e052e32be4392b3fcafae4d8f
Sahil Gedam <gedamsahil1997@gmail.com>
2023-07-11 17:05:26 +0530

    rowSpan project

--- project 2/NewProject/src/app/app.component.ts
+++ project 2/NewProject/src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title : string= 'NewProject'
+  title: string = 'NewProject';
   numberArray: number[] = [];
-
+  displayErrorMessage = '';
   numbersArrayColsPan: any[] = [];
   colspanTwoDarray: any[] = [];
   colsArrayForRows: any[] = [];
@@ -22,33 +22,72 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {}
 
   generateNumberArray() {
-    this.numbersArrayColsPan = JSON.parse('[' + this.form.colspan + ']');
+    try {
+      this.numbersArrayColsPan = JSON.parse('[' + this.form.colspan + ']');
+    } catch (error) {
+      this.displayErrorMessage =
+        'enter proper input only using numbers and comma in format  - number , number, number';
+    }
   }
-  clear(){
-    this.colspanTwoDarray=[]
-    this.numbersArrayColsPan=[]
-    this.numberArray=[]
-   
+  clear() {
+ 
+    this.form.colsPerRow = 0;
+    this.form.noOfInputs = 0;
+    this.form.colspan = '';
+    this.colspanTwoDarray = [];
+    this.numbersArrayColsPan = [];
+    this.colsArrayForRows = [];
+    this.numberArray = [];
+    this.displayErrorMessage = '';
+  }
+  clearOutPutOnChange() {
+    this.colspanTwoDarray = [];
+    this.numbersArrayColsPan = [];
+    this.numberArray = [];
+    this.displayErrorMessage = '';
+  }
+  checkErrorsFn(element: any, max: any) {
+    if (this.form.colsPerRow < 1) {
+      this.displayErrorMessage = 'colspan per row cannot be less than one';
+    } else if (element < 1) {
+      this.displayErrorMessage = 'colspan cannot be less than one';
+    } else if (element > max) {
+      this.displayErrorMessage =
+        'input colspan number is greater than max cols per row';
+    } else if (this.form.noOfInputs != this.numbersArrayColsPan.length) {
+      this.displayErrorMessage =
+        'no of input and no of colspan input does not match';
+    }
   }
 
-  generateComponents() {
-    this.generateNumberArray();
-    console.log(this.numbersArrayColsPan);
-
-    this.numberArray = Array(this.form.noOfInputs)
-      .fill(0)
-      .map((x, i) => i);
-
-      this.colsArrayForRows= Array(this.form.colsPerRow)
-      .fill(0)
-      .map((x, i) => i);
-
+  generateComponents(data: any): void {
+    this.form.colspan = data.colspan;
+    this.form.noOfInputs = data.noOfInputs;
+    this.form.colsPerRow = data.colsPerRow;
+    this.colspanTwoDarray = [];
+  
     let max = this.form.colsPerRow;
     let sum = 0;
     let tempArray: any[] = [];
 
+    this.generateNumberArray();
+ 
+    try {
+      this.numberArray = Array(this.form.noOfInputs)
+        .fill(0)
+        .map((x, i) => i);
+
+      this.colsArrayForRows = Array(this.form.colsPerRow)
+        .fill(0)
+        .map((x, i) => i);
+    } catch (error) {
+      console.log(error);
+    }
+
     for (let index = 0; index < this.numbersArrayColsPan.length; index++) {
       let element = this.numbersArrayColsPan[index];
+      this.checkErrorsFn(element, max);
+
       if (sum >= max) {
         this.colspanTwoDarray.push(tempArray);
         sum = 0;
--- project 2/NewProject/src/app/components/table/table.component.ts
+++ project 2/NewProject/src/app/components/table/table.component.ts
@@ -3,8 +3,96 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent {
-
+  form = {
+    noOfInputs: 0,
+    colsPerRow: 0,
+    rowspan: '',
+  };
+  numberArraryRowSpan: any[] = [];
+  numberArray: number[] = [];
+  rowSpanTwoDArray: any[] = [];
+  supportArrayForEachCols: any[] = [];
+  displayErrorMessage = '';
+  largestNumberOfInputs = 0;
+  clear() {
+    this.rowSpanTwoDArray = [];
+    this.form.noOfInputs = 0;
+    this.form.colsPerRow = 0;
+    this.form.rowspan = '';
+    this.numberArraryRowSpan = [];
+    this.numberArray = [];
+    this.supportArrayForEachCols = [];
+    this.displayErrorMessage = '';
+  }
+  clearOutPutOnChange() {
+    this.rowSpanTwoDArray = [];
+    this.numberArraryRowSpan = [];
+    this.numberArray = [];
+    this.supportArrayForEachCols = [];
+    this.displayErrorMessage = '';
+  }
+  generateNumberArray() {
+    try {
+      this.numberArraryRowSpan = JSON.parse('[' + this.form.rowspan + ']');
+      this.largestNumberOfInputs = Math.max.apply(0, this.numberArraryRowSpan);
+    } catch (error) {
+      this.displayErrorMessage =
+        'enter proper input only using numbers and comma in format  - number , number, number';
+    }
+  }
+  checkErrorsFn(element: any) {
+    if ((this.form.rowspan = '')) {
+      this.displayErrorMessage = 'enter input';
+    } else if (element < 0) {
+      this.displayErrorMessage = 'rowspan must be greater than 0';
+    } else if (this.form.colsPerRow < 1) {
+      this.displayErrorMessage = 'colsPerRow must be greater than 0';
+    } else if (this.form.noOfInputs < 1) {
+      this.displayErrorMessage = 'no of input cannot be less than 1';
+    } else if (this.form.noOfInputs != this.numberArraryRowSpan.length) {
+      this.displayErrorMessage = 'no of input and rowspan input does not match';
+    }
+  }
+  generateComponents(data: any): void {
+    this.rowSpanTwoDArray = [];
+    this.form.noOfInputs = data.noOfInputs;
+    this.form.colsPerRow = data.rowsPerCol;
+    this.form.rowspan = data.rowspan;
+    let max = this.form.colsPerRow;
+    this.generateNumberArray();
+    if(this.form.rowspan==''){
+      this.displayErrorMessage='please enter a proper input'
+    }
+    try {
+      this.numberArray = Array(this.form.noOfInputs)
+        .fill(0)
+        .map((x, i) => i);
+      this.supportArrayForEachCols = Array(this.largestNumberOfInputs)
+        .fill(0)
+        .map((x, i) => i);
+      this.supportArrayForEachCols.pop();
+    } catch (error) {
+      console.log(error);
+    }
+    let tempArray: any[] = [];
+    for (let index = 0; index < this.numberArraryRowSpan.length; index++) {
+      let element = this.numberArraryRowSpan[index];
+      this.checkErrorsFn(element);
+      tempArray.push(element);
+      if (tempArray.length == max) {
+        this.rowSpanTwoDArray.push(tempArray);
+        tempArray = [];
+      }
+      if (
+        index == this.numberArraryRowSpan.length - 1 &&
+        tempArray.length > 0
+      ) {
+        this.rowSpanTwoDArray.push(tempArray);
+        tempArray = [];
+      }
+    }
+  }
 }
